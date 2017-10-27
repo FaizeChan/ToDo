4f855771747e2c6907366310647e320e3daff88f
FaizeChan <obey.faize@gmail.com>
2017-10-27 10:23:20 -0400

    made a bool to finish off the Item.cs

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -9,7 +9,11 @@ namespace ToDo.Models
     public class Item
     {
         [Key]
-        public int ID { get; set; }
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Details { get; set; }
+        public bool IsDone { get; set; }
 
     }
 }
